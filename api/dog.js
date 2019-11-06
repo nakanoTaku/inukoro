924744b9a12faae26b49e35730bbebcc93edf023
opiyo <kosmo.waizu0804@gmail.com>
2019-11-06 22:08:28 +0900

    add dog_images save to state

--- api/dog.js
+++ api/dog.js
@@ -12,6 +12,19 @@ class DogApi {
       })
       .catch( e => ({ error: e }));
   }
+
+  dogs( breed ) {
+    return axios.get(`${this.apiBase}/breed/${breed}/images`)
+      .then( json => {
+        return json.data.message.map( (d) => {
+          return {
+            url: d,
+            like: 0,
+          };
+        });
+      })
+      .catch( e => ({ error: e }))
+  }
 }
 
 const dogApi = new DogApi();
--- store/index.js
+++ store/index.js
@@ -4,11 +4,16 @@ const appStore = () => {
   return new Vuex.Store({
     state: {
       breed_list: {},
+      dog_list: {},
     },
     mutations: {
       breed_list_update(state, payload) {
         state.breed_list = { ...payload }
       },
+
+      dog_list_update(state, payload) {
+        state.dog_list = { ...payload }
+      }
     }
   })
 };
